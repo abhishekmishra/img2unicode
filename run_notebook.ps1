86298f68a62b65fd519c5a60535e0f13da435df4
Abhishek Mishra <abhishekmishra3@gmail.com>
2019-10-08 18:21:37 +0530

    added run script

--- run_notebook.ps1
+++ run_notebook.ps1
@@ -1,1 +1,1 @@
-﻿docker run --rm -p 10000:8888 -v d:/code2/img2unicode:/home/jovyan/code jupyter/datascience-notebook:1386e2046833
+﻿docker run -it --rm -p 10000:8888 -v d:/code2/img2unicode:/home/jovyan/code jupyter/datascience-notebook:1386e2046833
